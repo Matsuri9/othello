2cba6d401f47edf926cb0ecdbd6fa4f464228490
1f10230057 <s1f102300571@iniad.org>
2023-04-28 14:37:40 +0900

    クリック検知だけ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,200 +1,217 @@
+// import './App.css';
 import styles from './index.module.css';
 
+// クリック判定
+
+function getClickedClassName(event: MouseEvent): string {
+  const clickedElement = event.target as HTMLElement;
+  return clickedElement.className;
+}
+
+// クリック時の処理を設定
+if (typeof window === 'object') {
+  document.addEventListener('click', (event) => {
+    const clickedClassName = getClickedClassName(event);
+    console.log(clickedClassName);
+  });
+}
+
+// 表示
 const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         <div className={styles.box}>
-          <div className={styles.discblack} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
         <div className={styles.box}>
-          <div className={styles.discwhite} />
+          <div className={styles.disc} />
         </div>
       </div>
     </div>
