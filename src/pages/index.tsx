13f1aaf5546e2ff8964e885975c75089a20d29bc
1f10230057 <s1f102300571@iniad.org>
2023-04-28 16:30:17 +0900

    [+]番地表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,22 @@
 import styles from './index.module.css';
 
 // クリック判定
+
 if (typeof window === 'object') {
   const elements = document.querySelectorAll('div');
   elements.forEach((element) => {
     element.addEventListener('click', () => {
       if (element.classList.value === 'index_disc__JTT4d') {
         const rect = element.getBoundingClientRect();
-        const centerX = rect.x + rect.width / 2;
-        const centerY = rect.y + rect.height / 2;
-        console.log(`中心の座標は x:${centerX} y:${centerY} です`);
+        const centerX = Math.floor(rect.x + rect.width / 2);
+        const centerY = Math.floor(rect.y + rect.height / 2);
+        const discNumber = ((centerY - 12) / 80 - 1) * 8 + (centerX - 130) / 80;
+        console.log(`指定されたDiscの番地は ${discNumber} です`);
       }
     });
   });
 }
+
 // 表示
 const Home = () => {
   return (
