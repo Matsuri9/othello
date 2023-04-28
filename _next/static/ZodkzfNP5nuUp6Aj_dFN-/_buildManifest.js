4730607d7c49a5dc09f4124aa38720503868cfef
s1f102300571 <s1f102300571@users.noreply.github.com>
2023-04-28 05:38:32 +0000

    deploy: 2cba6d401f47edf926cb0ecdbd6fa4f464228490

--- _next/static/ZodkzfNP5nuUp6Aj_dFN-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4bc43dd41b4030fe.css","static/chunks/pages/index-52a4ea655603258f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
