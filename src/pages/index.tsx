c53309c51b711a269923bf01820eb13e48c4747a
1f10230057 <s1f102300571@iniad.org>
2023-04-30 13:38:24 +0900

    [+]周囲の状態を判定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ interface Box {
 }
 
 const Home = () => {
+  const [whiteTurn, setWhiteTurn] = useState<boolean>(true);
   const [boxes, setBoxes] = useState<Box[]>(
     Array.from(Array(64), (_, index) => ({
       id: index + 1,
@@ -18,9 +19,40 @@ const Home = () => {
     }))
   );
 
+  // id = 番号
+  // clickedBox
+
   const handleClickBox = (id: number) => {
     const clickedBox = boxes.find((box) => box.id === id);
-    console.log(`Box ${id} is in state: ${clickedBox?.state || 'not found'}`);
+    const clickedState = clickedBox?.state;
+    console.log(`Box ${id} is in state: ${clickedState || 'not found'}`);
+
+    const functionNumber = [-8, -7, 1, 9, 8, 7, -1, -9];
+
+    for (let t = 0; t < functionNumber.length; t++) {
+      let tempBox = id;
+      for (let i = 0; i < 10; ) {
+        tempBox += functionNumber[t];
+        if (
+          tempBox <= 0 ||
+          65 <= tempBox ||
+          ((t === 1 || t === 2 || t === 3) && (tempBox - 1) % 8 === 0) ||
+          ((t === 5 || t === 6 || t === 7) && tempBox % 8 === 0)
+        ) {
+          console.log('break');
+          break;
+        }
+        const tempBoxState = boxes.find((box) => box.id === tempBox)?.state;
+        if (clickedState === 'empty' && whiteTurn) {
+          setWhiteTurn(false);
+        } else if (clickedState === 'empty' && !whiteTurn) {
+          setWhiteTurn(true);
+        }
+        console.log(`Box ${tempBox} is in state: ${tempBoxState} : ${t}`);
+      }
+    }
+
+    // call changeBoxState function
     changeBoxState(id, 'black');
   };
 
